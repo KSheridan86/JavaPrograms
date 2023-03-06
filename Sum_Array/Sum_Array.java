a954cf71f8019c7067ed9f46b82e858a252b2bff
Ken Sheridan <kensheridan86@gmail.com>
2023-03-06 15:02:11 +0000

    week 8 labs added

---
+++ Sum_Array/Sum_Array.java
@@ -0,0 +1,41 @@
+package Sum_Array;
+/*
+Ken Sheridan
+Sum_Array.java
+06/03/2023
+*/
+
+// Program to sum the rows and columns of a 3x3 array
+public class Sum_Array {
+    public static void main(String[] args) {
+        // Create a 3x3 array and fill it with values
+        int[][] array = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+        // Calculate the sum of each row
+        for (int i = 0; i < array.length; i++) {
+            int rowSum = 0;
+            for (int j = 0; j < array[i].length; j++) {
+                rowSum += array[i][j];
+            }
+            System.out.println("Sum of row " + (i+1) + ": " + rowSum);
+        }
+
+        // Calculate the sum of each column
+        for (int i = 0; i < array[0].length; i++) {
+            int colSum = 0;
+            for (int j = 0; j < array.length; j++) {
+                colSum += array[j][i];
+            }
+            System.out.println("Sum of column " + (i+1) + ": " + colSum);
+        }
+
+        // sum entire array
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                sum += array[i][j];
+            }
+        }
+        System.out.println("Sum of entire array: " + sum);
+    }
+}
