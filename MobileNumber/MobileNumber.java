8423462d33dcd30e944e1b31058a8d4940211e46
Ken Sheridan <kensheridan86@gmail.com>
2023-03-16 20:09:00 +0000

    mobile number updated

--- MobileNumber/MobileNumber.java
+++ MobileNumber/MobileNumber.java
@@ -1,13 +1,13 @@
 package MobileNumber;
-/*
-MobileNumber.java
-Ken Sheridan
-16/03/23
-*/
+// /*
+// MobileNumber.java
+// Ken Sheridan
+// 16/03/23
+// */
 
-// Create an application to check for a valid mobile phone number. 
-// A valid mobile phone number is 10 digits in total and should start with either, 083, 085 or 087. 
-// The application should ask the user for their mobile number and output if the number is valid or invalid.
+// // Create an application to check for a valid mobile phone number. 
+// // A valid mobile phone number is 10 digits in total and should start with either, 083, 085 or 087. 
+// // The application should ask the user for their mobile number and output if the number is valid or invalid.
 
 public class MobileNumber {
     // data Members
@@ -25,12 +25,25 @@ public class MobileNumber {
     // compute
     public void compute() {
         // check if number is 10 digits
-        if (number.length() <= 10) {
-            
+
+if (number.length() == 10) { //if the number has a length of 10
+    //if the number has a character at index zero and its equal to a zero, do the following
+    if ((number.charAt(0) == '0') && (number.charAt(1) == '8')) {
+        if (number.charAt(2) == '3' || number.charAt(2) == '5' || number.charAt(2) == '7') {
+            message = "This is a valid number";
+        } else {
+            message = "This is not a valid number";
+        }
+    }
+}
+
+
+        if (number.length() == 10) {
+
             // if (number.charAt(0) == '0' && number.charAt(1) == '8') {
             //      if (number.charAt(2) == '3' || number.charAt(2) == '5' || number.charAt(2) == '7') {
             //          message = "Valid"; }
-            
+
             if (number.startsWith("083") || number.startsWith("085") || number.startsWith("087")) {
                 message = "Valid";
             } else {
@@ -45,3 +58,48 @@ public class MobileNumber {
         return message;
     }
 }
+
+
+// public class MobileNumber {
+
+//     //Data members
+
+//     private String number; //input
+//     private String message; //Output
+
+//     //constructor
+//     public MobileNumber() {
+//         number = "";
+//         message = "";
+//     }
+
+//     //Setter
+//     public void setNumber(String number) {
+//         this.number = number;
+//     }
+
+//     //Compute
+//     public void compute() {
+//         //check for the length
+//         if (number.length() == 10) { //if the number has a length of 10
+//             //if the number has a character at index zero and its equal to a zero, do the following
+//             if ((number.charAt(0) == '0') && (number.charAt(1) == '8')) {
+//                 if (number.charAt(2) == '3' || number.charAt(2) == '5' || number.charAt(2) == '7') {
+//                     message = "This is a valid number";
+//                 } else {
+//                     message = "This is not a valid number";
+//                 }
+//             }
+//         }
+//     }
+
+//     //Getter
+//     public String getMessage() {
+//         return message;
+//     }
+
+
+
+
+
+// }
--- MobileNumber/MobileNumberApp.java
+++ MobileNumber/MobileNumberApp.java
@@ -14,8 +14,38 @@ public class MobileNumberApp {
 
         System.out.print("Enter your mobile number: ");
         number = keyboard.nextLine();
+        myMobileNumber.setNumber(number);
         myMobileNumber.compute();
         message = myMobileNumber.getMessage();
         System.out.println(message);
+        keyboard.close();
     }
 }
+
+/*
+MobileNumberApp
+L Murphy
+16.3.2023
+*/
+
+// import java.util.*;
+// public class MobileNumberApp{
+// 	public static void main(String args[]){
+
+// 		String number, message;
+
+// 		Scanner keyboard=new Scanner(System.in);
+// 		MobileNumber myMobileNumber=new MobileNumber();
+
+
+// 		System.out.println("Please enter a mobile number");
+// 		number=keyboard.nextLine();
+// 		myMobileNumber.setNumber(number);
+
+// 		myMobileNumber.compute();
+
+// 		message=myMobileNumber.getMessage();
+// 		System.out.println(message);
+//         keyboard.close();
+// 	}
+// }
